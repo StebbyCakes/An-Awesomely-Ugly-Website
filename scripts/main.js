26ecf3be622370013aef9879e7095addac946770
Esteban Carter <carterea1999@gmail.com>
2021-05-24 13:44:10 -0400

    Final Product w/ working button that Changes Img

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
-var button = document.getElementbyId('button');
-
-button.addEventListener('click', function(button){
-  button.target.classList.toggle('newimg')
-});
+function changeImg() {
+        var newI = "https://bloximages.newyork1.vip.townnews.com/heraldcourier.com/content/tncms/assets/v3/editorial/a/fd/afd880d4-9de3-11e5-9f88-177225123eb9/5667326dc542c.image.jpg"
+        document.getElementById("image").src = newI;
+      }
